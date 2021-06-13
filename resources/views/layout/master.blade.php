4bc11607d676edb4d3c263a08df6e97366750b08
Pongsiri Reeveerakul <61234036@cmru.ac.th>
2021-06-13 15:51:34 +0700

    Finishedzz

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <b><center>
-        <a href="/">Main</a> | <a href="/about">About</a> | <a href="/contact">Contact</a>
+        <a href="/">Main</a> | <a href="/about">About</a> | <a href="/contact">Contact</a> | <a href="/calculator">Contact</a>
     </center></b>
     <hr>
     @yield('content')
